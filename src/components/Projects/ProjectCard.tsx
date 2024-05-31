490dd871c10743297c1a524ee324572ec93961e3
codeversepro <codeversepro@gmail.com>
2024-05-31 16:36:54 +0530

    hover over project card scale

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -8,7 +8,7 @@ type TProjectCard = {
 export default function ProjectCard({ projectImg, projectTitle }: TProjectCard) {
     const themeContext = useThemeContext();
     return (
-        <div className="flex flex-col justify-center items-center gap-5">
+        <div className="flex flex-col justify-center items-center gap-5 lg:hover:scale-105 transition-all duration-75">
             <img src={projectImg} className={`bg-white rounded-lg w-[200px] h-[200px]  shadow-sm  ${themeContext.theme == 'dark' ? "border-pista-500 shadow-pista-200" : "border-dgreen-700 shadow-dgreen-300"}`} alt="#" />
             <h3 className="font-semibold text-lg">{projectTitle}</h3>
         </div>
